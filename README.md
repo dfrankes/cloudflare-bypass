387bbea39818fd920124a1758956538d94dca5e0
dfrankes <dfrankes@users.noreply.github.com>
2021-12-11 16:41:07 +0100

    Project Setup

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # cloudflare-bypass
- A NodeJS package to bypass the Cloudflare UAM using puppeteer
+ A very basic Cloudflare UAM bypass (ideal for scrapers)
+ Using puppeteer (a chromium based headless browser) to resolve the UAM check
+
+## Usage
+``` js
+const cloudflareBypass = require('cloudflare-bypass');
+(async function(){
+    const cookie = await cloudflareBypass({
+        url: 'https://cloudflare-protected-url.com'
+    });
+
+    console.log(`Cloudflare bypass cookie`, cookie);
+})();
+```
+
+### Output
+``` json
+{
+  name: 'cf_clearance',
+  value: 'Example value',
+  domain: '.cloudflare-protected-url.com',
+  path: '/',
+  expires: 1670776181.662808,
+  size: 72,
+  httpOnly: true,
+  secure: true,
+  session: false,
+  sameSite: 'None',
+  priority: 'Medium',
+  sameParty: false,
+  sourceScheme: 'Secure',
+  sourcePort: 443
+}
+```
+
+## TODO
+- Add captcha resolver
---
+++ src/cloudflare-bypass.js
@@ -0,0 +1,41 @@
+const puppeteer = require('puppeteer-extra');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+puppeteer.use(StealthPlugin());
+
+module.exports = async function (config) {
+    const { url } = config;
+
+    // Create the browser instance
+    const browser = await puppeteer.launch({
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        headless: false,
+    });
+
+    // Create a new page instance
+    const target = await browser.newPage();
+
+    // Visit target url and read out the body
+    await target.goto(url);
+    const pageBody = await target.evaluate(() => document.querySelector('*').outerHTML);
+
+    // Validate if we hit the cloudflare UAM page
+    if (pageBody.includes('cf-im-under-attack')) {
+
+        // Wait for the cf_clearance cookie
+        const clearance = await new Promise((resolve, reject) => {
+            let count = 0;
+            target.on('request', async (request) => {
+                const { cookies } = await target._client.send('Network.getAllCookies');
+                let cf_clearance = cookies.findIndex((c) => c.name === 'cf_clearance');
+                if (cf_clearance > -1) {
+                    cf_clearance = cookies[cf_clearance];
+                    resolve(cf_clearance);
+                }
+            });
+        });
+
+        await target.close();
+        await browser.close();
+        return clearance;
+    }
+};
